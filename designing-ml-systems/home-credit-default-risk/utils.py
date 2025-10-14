92670ed81855c897ccdaf9d3de2f71e88b207140
Nicole Ye <nicoleyxye@gmail.com>
2025-10-13 21:13:03 -0400

    Make hyper param tuning optional
    Bc I lost patience

--- designing-ml-systems/home-credit-default-risk/utils.py
+++ designing-ml-systems/home-credit-default-risk/utils.py
@@ -1,6 +1,8 @@
 import json
 import joblib
-
+import pandas as pd
+import numpy as np
+from sklearn.metrics import make_scorer, roc_auc_score, f1_score, confusion_matrix, precision_recall_curve
 
 def save_best_model(grid, output_path):
     best_model = grid.best_estimator_
@@ -17,3 +19,19 @@ def save_best_model(grid, output_path):
         json.dump(meta, f, indent=2)
 
     print(f"✅ Metadata saved to {json_path}")
+
+
+#Testing scores, confusion matrix
+def model_performance(y_test,y_pred,y_pred_pr):
+    print("f1_score: ", f1_score(y_test, y_pred))
+    print("confusion matrix", confusion_matrix(y_test,y_pred))
+    print("roc_auc_score: ", roc_auc_score(y_test,y_pred_pr))
+
+
+#print result, draw bar graph for feature importance for non-pca method
+def print_result(model,x_test):
+    print(model)
+    feat_importances = pd.Series(model.best_estimator_.feature_importances_, index=x_test.columns)
+    feat_importances.nlargest(15).plot(kind='barh')
+    return pd.DataFrame(model.cv_results_)
+
